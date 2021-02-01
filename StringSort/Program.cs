998c812a3d9f3c628eeb6efb06d68d41d249575d
datta1234 <datta3343@gmail.com>
2021-02-01 15:45:49 +0200

    first commit

---
+++ StringSort/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StringSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter string to be sorted");
+                string input = Console.ReadLine();
+                stringsort stringSort = new stringsort();
+                int[] ascciArray = stringSort.GetAsciiArrayofString(input);
+                Console.Write(stringSort.SortArray(ascciArray, ascciArray.Length, 97, 122));
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
